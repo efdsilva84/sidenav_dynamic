5d18e608b0b3102662fdae5001477bb3477dbeb6
edcarlos <efdsilva84@gmail.com>
2023-10-22 00:44:26 -0400

    rotação btn x side nav

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 import { navbarData } from './nav-data';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 interface SideNavToggle{
   scrrenWidth: number;
@@ -9,7 +10,33 @@ interface SideNavToggle{
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
+  animations:[
+    trigger('fadeInOut',[
+      transition(':enter', [
+        style({opacity:0}),
+        animate('350ms', style({opacity:0})
+        )
+      ]),
+      transition(':leave',[
+        style({opacity:1}),
+        animate('350ms',
+        style({opacity:0})
+        )
+      ])
+    ]),
+    trigger('rotate',[
+      transition(':enter',[
+        animate('1000ms',
+        keyframes([
+          style({transform:'rotate(0deg)', offset:'0'}),
+          style({transform:'rotate(2turn)', offset:'1'}),
+
+        ])
+        )
+      ])
+    ])
+  ]
 })
 export class SidenavComponent implements OnInit {
 
