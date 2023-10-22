086d3ab44f4eb553e07db6688d3771385a1e68b1
edcarlos <efdsilva84@gmail.com>
2023-10-22 14:07:33 -0400

    start service, config service, config api

--- src/app/sidenav/nav-data.ts
+++ src/app/sidenav/nav-data.ts
@@ -1,17 +1,17 @@
 export const navbarData=[
   {
     routeLink: 'usuarios',
-    icon: 'fa-solid fa-house',
+    icon: 'fa-solid fa-users-line',
     Label: 'Usuários'
   },
   {
     routeLink: 'registros',
-    icon: 'fa-solid fa-house',
+    icon: 'fa-solid fa-folder-open',
     Label: 'Registros'
   },
   {
     routeLink: 'credenciais',
-    icon: 'fa-solid fa-house',
+    icon: 'fa-solid fa-address-card',
     Label: 'Credenciais'
   }
 ]
